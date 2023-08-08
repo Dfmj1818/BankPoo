1ad131707996b796e846e03f665d3f73ec5a3cac
Daniel Malaver <danieljimenezgg@gmail.com>
2023-08-08 16:33:52 -0500

    Añadiendo clase quota

---
+++ src/Model/Quota.java
@@ -0,0 +1,77 @@
+package Model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Quota {
+	private int quotaId;	
+	private long quotaAmount;
+	private LocalDate startDate;
+	private LocalDate dueDate;
+	private boolean stateOfQuota;
+	private LocalDate dayOfPayment;
+	private LocalDate maxDueDate;
+	public Quota(){
+		this.quotaId=quotaId;
+		this.quotaAmount=quotaAmount;  
+		this.startDate=startDate;
+		this.dueDate=dueDate;
+	}
+
+	public void setQuotaId(int quotaId){
+		this.quotaId=quotaId;
+	}
+
+	public int getQuotaID() {
+		return quotaId;
+	}
+
+	public void setQuotaAmount(long quotaCost){
+		this.quotaAmount=quotaCost;
+	}
+
+	public long getQuotaAmount() {
+		return quotaAmount;
+	}  
+
+	public void setStartDate(LocalDate startDate ){
+		this.startDate=startDate;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate=dueDate; 
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+	
+	public void setMaxDueDate(LocalDate maxDueDate){
+		this.maxDueDate=maxDueDate;
+	}
+	
+	public LocalDate getMaxDueDate() {
+		return maxDueDate;
+	}
+
+	public void setStateOfQuota(boolean stateOfQuota) {
+		this.stateOfQuota=stateOfQuota;
+	}
+
+	public boolean getStateOfQuota(){
+		return stateOfQuota;
+	}
+
+	public void setDayOfPayment(LocalDate dateOfPayment) {
+		this.dayOfPayment=dateOfPayment;
+	}
+
+	public LocalDate getDayOfPayment() {
+		return dayOfPayment;
+	}
+	
+}
